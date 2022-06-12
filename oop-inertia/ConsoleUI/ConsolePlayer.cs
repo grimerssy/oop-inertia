3fed2ef2e9a4637f38ef70d6dda6b5bad8f8d58d
grimerssy <stanislav.stoianov@nure.ua>
2022-06-12 21:29:16 +0300

    (chore): fix directories layout and project references

--- oop-inertia/ConsoleUI/ConsolePlayer.cs
+++ oop-inertia/ConsoleUI/ConsolePlayer.cs
@@ -5,7 +5,7 @@ namespace ConsoleUI;
 
 public class ConsolePlayer : Player
 {
-    public ConsoleColor Color;
+    public readonly ConsoleColor Color;
     
     public ConsolePlayer(string name, Inertia.Field.Field field, Coordinate coordinate, ConsoleColor color) : base(name, field, coordinate)
     {
