525f5d415fd771c4d500c7d31217775041d40270
grimerssy <stanislav.stoianov@nure.ua>
2022-06-24 15:39:14 +0300

    add home page

--- oop-inertia/WebUI/src/routes/index.svelte
+++ oop-inertia/WebUI/src/routes/index.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import preview from '../static/placeholder.svg'
+</script>
+
+<div>
+    <div>
+        <img src={preview} alt="preview">
+    </div>
+    <div>
+        <a sveltekit:prefetch href="/game">Play</a>
+    </div>
+</div>
+
+<style>
+    div {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+    }
+
+    div div {
+        width: 50%;
+    }
+
+    img {
+        max-width: 250px;
+        width: 80%;
+    }
+
+    a {
+        font-size: 2rem;
+        padding: 1rem;
+        background: var(--main-white);
+        border-radius: 0.5rem;
+    }
+</style>
